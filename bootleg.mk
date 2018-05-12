39b74f740fb5c498d0df5acbfbaaf3361c9c53cd
amog787 <amoghmaiya@gmail.com>
2018-05-12 08:10:17 +0000

    Bootleg Bringup

---
+++ bootleg.mk
@@ -0,0 +1,51 @@
+# Copyright (C) 2013-2016, The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from z2_plus device
+$(call inherit-product, device/zuk/z2_plus/device.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+
+# Boot animation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+BOOTLEG_BUILD_TYPE := Shishufied
+# Inherit some common AOSP-OMS stuff.
+#$(call inherit-product, vendor/aosp/common.mk)
+
+PRODUCT_NAME := bootleg_z2_plus
+PRODUCT_DEVICE := z2_plus
+PRODUCT_MANUFACTURER := ZUK
+PRODUCT_BRAND := ZUK
+PRODUCT_MODEL := Z2 Plus
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=z2_plus PRODUCT_NAME=bootleg_z2_plus
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="z2_plus-user 7.0 NRD90M 3.1.117_171110 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "ZUK/z2_plus/z2_plus:7.0/NRD90M/3.1.117_171110:user/release-keys"
+
+# Bootleg Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="amog787"
+
+# Show build as release(to pass CTS without Magisk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_VERSION_TAGS="release-keys"
